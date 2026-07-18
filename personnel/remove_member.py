b4b3e1019aaa25a1cf5864b968a243ae1f2bdbdf
Boss <195664763+Boss-6651@users.noreply.github.com>
2026-07-18 12:21:10 +0700

    removemember

--- personnel/remove_member.py
+++ personnel/remove_member.py
@@ -1,11 +1,25 @@
 # =====================================================
-#  personnel/remove_member.py — คนรับผิดชอบ: ______________________
+#  personnel/remove_member.py — คนรับผิดชอบ: Boss
 # =====================================================
 from data import family_members
 
 # TODO: สร้างฟังก์ชัน remove_member(target_name)
 #   - หาคนที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก) แล้วลบออกจาก family_members
 #   - ลบสำเร็จ -> return True | ไม่เจอ -> return False
+
+
+def remove_member(target_name):
+    target_name = input("ชื่อลูกน้องที่อยากเก็บ: ").lower()
+
+
+    for i in family_members:
+        if target_name == i["name"].lower():
+            family_members.remove(i)
+            return "สั่งเก็บลูกน้องเรียบร้อยแล้ว"
+        else:
+           return "ไม่พบชื่อในระบบ"
+
+
 
 
 # ทดสอบ: python -m personnel.remove_member
